1f4def21fce5bdbbfe9b26f1bfb61dcdcaeb6006
Muugii Munkhbold <mmunkhbold94@gmail.com>
2023-01-02 16:11:52 -0700

    worked on views folder and public folder

--- models/index.js
+++ models/index.js
@@ -1,11 +1,34 @@
 const User = require('./User');
 const Comment = require('./Comment');
 const Post = require('./Post');
+const { Model } = require('sequelize');
+const { default: ModelManager } = require('sequelize/types/model-manager');
 
-User.hasMany(Income, {
+User.hasMany(Post, {
     foreignKey: 'user_id',
-    onDelete: 'CASCADE'
 });
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+});
+
+User.hasMany(Comment, {
+    foreignKey: `post_id`,
+});
+
+Comment.belongsTo(User, {
+    foreignKey: `post_id`,
+});
+
+module.exports = { User, Post, Comment };
 
 
 
